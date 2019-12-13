7076aa4fe1e0c4d24b833677be5776fee3932b7e
ThaliaM <thalia.manrike@gmail.com>
2019-12-13 00:41:50 -0500

    initial commit

---
+++ src/app/app.routes.ts
@@ -0,0 +1,28 @@
+import { RouterModule, Routes } from '@angular/router';
+
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { LoginComponent } from './login/login.component';
+import { ProgressComponent } from './pages/progress/progress.component';
+import { Graficas1Component } from './pages/graficas1/graficas1.component';
+import { NopagefoundComponent } from './shared/nopagefound/nopagefound.component';
+import { PagesComponent } from './pages/pages.component';
+import { RegisterComponent } from './login/register.component';
+
+// arreglo de rutas, el path es la url
+const appRoutes: Routes = [
+    {
+        path: '',
+        component: PagesComponent,
+        children: [
+            { path: 'dashboard', component: DashboardComponent },
+            { path: 'progress', component: ProgressComponent },
+            { path: 'graficas1', component: Graficas1Component },
+            { path: '', redirectTo: '/dashboard', pathMatch: 'full'}, // cualquier ruta vacia redirecciona a /dasboard 
+        ]
+    },
+    { path: 'login', component: LoginComponent },
+    { path: 'register', component: RegisterComponent },
+    { path: '**', component: NopagefoundComponent } // cualquier ruta no definida mostrara componente nopagefound
+];
+
+export const APP_ROUTES = RouterModule.forRoot(appRoutes, {useHash: true}); // forRoot() porque son rutas principales
